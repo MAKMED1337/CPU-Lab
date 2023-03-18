649459f597470218edc4013e2a12881905a3ff25
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-03-18 06:21:45 +0200

    Added method remap, to remap args to values, useful with new instruction translator

--- ASM/InstructionList.cpp
+++ ASM/InstructionList.cpp
@@ -13,19 +13,64 @@ namespace ASM {
 		
 		WORD ptr = 0;
 		for(auto instruction : m_instructions) {
-			std::visit([&](auto i) {
-				using T = std::decay_t<decltype(i)>;
+			std::visit([&](auto instr) {
+				using T = std::decay_t<decltype(instr)>;
 				if constexpr (std::is_same_v<T, Instruction>)
-					memory[ptr++] = std::to_underlying(i);
+					memory[ptr++] = std::to_underlying(instr);
 				else if constexpr (std::is_same_v<T, WORD>)
-					memory[ptr++] = i;
+					memory[ptr++] = instr;
 				else if constexpr (std::is_same_v<T, Arg>)
-					memory[ptr++] = args[i.ind];
+					memory[ptr++] = args[instr.ind];
 				else
 					static_assert(always_false_v<T>, "Unknown type");
 			}, instruction);
 		}
 		
 		return ptr;
+	}
+	
+	InstructionList& InstructionList::extend(const InstructionList& other) {
+		m_instructions.insert(end(m_instructions), other.m_instructions.begin(), other.m_instructions.end());
+		return *this;
+	}
+	
+	size_t InstructionList::get_args_count() const {
+		return args_count;
+	}
+	
+	InstructionList::InstructionList(std::vector<constructor_type> instructions, size_t arguments_count)
+		: args_count(arguments_count) {
+		for (auto const& instruction : instructions) {
+			std::visit([&](auto const& i) {
+				using T = std::decay_t<decltype(i)>;
+				if constexpr (std::is_same_v<T, InstructionList>)
+					extend(i);
+				else
+					m_instructions.push_back(i);
+			}, instruction);
+		}
+	}
+	
+	WORD InstructionList::size() const {
+		return m_instructions.size();
+	}
+	
+	InstructionList InstructionList::remap(const std::vector<argument_type>& args) {
+		assert(args_count == args.size());
+		
+		std::vector<instruction_type> res(m_instructions.size());
+		for(size_t i = 0; i < res.size(); ++i) {
+			std::visit([&](auto instr) {
+				using T = std::decay_t<decltype(instr)>;
+				if constexpr (std::is_same_v<T, Arg>) {
+					auto const& arg = args[instr.ind];
+					if (std::holds_alternative<WORD>(arg))
+						res[i] = std::get<WORD>(arg);
+					else
+						res[i] = std::get<Arg>(arg);
+				} else
+					res[i] = instr;
+			}, m_instructions[i]);
+		}
 	}
 }
--- ASM/InstructionList.hpp
+++ ASM/InstructionList.hpp
@@ -11,13 +11,20 @@ namespace ASM {
 	};
 	
 	class InstructionList final {
-		using type = std::variant<Instruction, WORD, Arg>;
-		std::vector<type> m_instructions;
+		using instruction_type = std::variant<Instruction, WORD, Arg>;
+		std::vector<instruction_type> m_instructions;
+		size_t args_count;
 	public:
-		const unsigned args_count;
+		size_t get_args_count() const;
+		WORD size() const;
 		
-		InstructionList(std::vector<type> instructions = {}, unsigned arguments_count = 0) :
-			m_instructions(std::move(instructions)), args_count(arguments_count) {}
+		using constructor_type = std::variant<Instruction, WORD, Arg, InstructionList>;
+		InstructionList(std::vector<constructor_type> instructions = {}, size_t arguments_count = 0);
+		
+		using argument_type = std::variant<WORD, Arg>;
+		InstructionList remap(std::vector<argument_type> const& args = {});
+		
+		InstructionList& extend(InstructionList const& other);
 		
 		//returns count of instructions
 		WORD push_instructions(std::vector<WORD>&& args, std::span<WORD> memory) const;
