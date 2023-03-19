efda51e59e61b9842fb966c8dae82e2848e67eda
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-03-20 00:30:58 +0200

    Added 2 JUMP related instructions Added comments with registers that changed during instruction Changed `PRINT` to print register `B`

--- ASM/Instructions.cpp
+++ ASM/Instructions.cpp
@@ -2,23 +2,30 @@
 
 namespace ASM {
 	void init() {
-#define INSTR(name, args...) InstructionList name { args }; translation[#name] = name;
-		INSTR(SAVE_B, { SWAP, SWAP_C })
-		INSTR(LOAD_B, { SWAP_C, SWAP })
-		INSTR(SWAP_BC, { SWAP, SWAP_C, SWAP })
+#define INTERNAL(name, args...) InstructionList name { args };
+#define INSTR(name, args...) INTERNAL(name, args) translation[#name] = name;
+		INSTR(SAVE_B, { SWAP, SWAP_C }) //A, C
+		INSTR(LOAD_B, { SWAP_C, SWAP }) //A, C
+		INSTR(SWAP_BC, { SWAP, SWAP_C, SWAP })  //A <-> B
+		
+		INSTR(JUMP, { SET, Arg{0}, WRITE_IP }, 1) //A
+		INSTR(JUMP_NOT_ZERO, { LNOT, JUMP_ZERO, Arg{0} }, 1) //A
 		
 		INSTR(STOP, { SET, Hardware::Processor::STOP, WRITE_IP })
+		INSTR(COPY_B, { SET, WORD{0}, ADD }) //A <- B
+		INSTR(COPY_A, { SWAP,COPY_B }) //B <- A
 		
-		INSTR(PRINT, { SWAP, SWAP_C, SET, Hardware::IO_OFFSET, WRITE, LOAD_B })
+		INSTR(PRINT, { SET, Hardware::IO_OFFSET, WRITE }) //A
 		
-		INSTR(READ_RAM, { ADD, Hardware::RAM_OFFSET, READ }) //0 indexed ram
-		INSTR(WRITE_RAM, { ADD, Hardware::RAM_OFFSET, WRITE }) //0 indexed ram
+		INSTR(READ_RAM, { ADD, Hardware::RAM_OFFSET, READ }) //A, 0 indexed ram
+		INSTR(WRITE_RAM, { ADD, Hardware::RAM_OFFSET, WRITE }) //A, 0 indexed ram
 		
-		INSTR(READ_STACK, { READ_SP, READ })
-		INSTR(WRITE_STACK, { READ_SP, WRITE })
+		INSTR(READ_STACK, { READ_SP, READ }) //A
+		INSTR(WRITE_STACK, { READ_SP, WRITE }) //A
 		
-		INSTR(INCREASE_STACK, { SAVE_B, SET, WORD{1}, SWAP, READ_SP, ADD, WRITE_SP, LOAD_B })
-		INSTR(POP_STACK, { SAVE_B, SET, WORD{1}, SWAP, READ_SP, SUB, WRITE_SP, LOAD_B })
+		INSTR(EXTEND_STACK, { SAVE_B, SET, WORD{1}, SWAP, READ_SP, ADD, WRITE_SP, LOAD_B }) //A, C
+		INSTR(PUSH_STACK, { EXTEND_STACK, WRITE_STACK }) //A, C
+		INSTR(POP_STACK, { SAVE_B, SET, WORD{1}, SWAP, READ_SP, SUB, WRITE_SP, LOAD_B }) //A, C
 		
 		InstructionList call_without_ip {{ ADD, SWAP, INCREASE_STACK, WRITE_STACK, SET, Arg{0}, WRITE_IP }, 1};
 		INSTR(CALL, { SET, call_without_ip.size(), SWAP, READ_IP, call_without_ip }, 1) //side effect on B
