cd233c705c78eb1147f07fcef15e6cc4f37b367d
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-05-17 23:16:48 +0200

    Used `Loader` to load code Added lambda `execute_step`

--- main.cpp
+++ main.cpp
@@ -2,25 +2,30 @@
 #include <fstream>
 #include <sstream>
 #include "ASM/Compiler.hpp"
-#include "ASM/Source.hpp"
 #include "Hardware/Processor.hpp"
 
 using Hardware::WORD;
 int main() {
 	ASM::init();
 
-	std::ifstream source_file("../code.txt"); //because .exe file in cmake-build-... folder
-	std::ostringstream buffer;
-	buffer << source_file.rdbuf();
-	std::string code = buffer.str();
-	
-	ASM::Compiler compiler;
-	compiler.add_source(ASM::Source{0, code});
+	std::shared_ptr<ASM::Loader> loader{new ASM::Loader{".."}};
+	ASM::Compiler compiler(loader);
+	compiler.add_source(loader->get_id("code.txt"));
 
 	Hardware::Computer computer(compiler.get_memory());
 
-	computer.execute_step([&](WORD index) { return compiler.get_mapping(index); });
-	auto segment = compiler.get_mapping(computer.processor->get_IP());
-	std::cerr << "ASM code(source_id = " << segment.source_id << "): " << code.substr(segment.from, segment.to - segment.from) << "\n";
-	//need to be second instruction, because first already done. (SET 42069)
+	auto execute_step = [&] {
+		if(computer.processor->complete())
+			return;
+
+		auto segment = compiler.get_mapping(computer.processor->get_IP());
+		auto code = loader->load(segment.source_id);
+		std::cerr << "ASM code(source_id = " << segment.source_id << "): " << code.substr(segment.from, segment.to - segment.from) << "\n";
+
+		computer.execute_step([&](WORD index) { return compiler.get_mapping(index); });
+	};
+
+	//while(!computer.processor->complete())
+	//	execute_step();
+	computer.execute(10000);
 }
