11466812ca999bd076ea44a83ce6ce9b3703d89e
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-05-17 02:09:30 +0200

    Moved code out from cpp file

--- main.cpp
+++ main.cpp
@@ -1,73 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include "ASM/Compiler.hpp"
 #include "Hardware/Processor.hpp"
 
 int main() {
 	ASM::init();
-	
-	constexpr std::string_view asm_code = R"code(
-PREPARE_ARGS
-SET 42069
-SWAP
-PUSH_STACK
-CALL :print_num 1
-STOP
 
-:print_num #NOTE: all numbers are unsigned, so -1 = 4294967295
-	READ_STACK
-	JUMP_NOT_ZERO :recall
-	SET '0'
-	SWAP
-	PRINT
-	POP_STACK
-	RET
+	std::ifstream source_file("../code.txt"); //because .exe file in cmake-build-... folder
+	std::ostringstream buffer;
+	buffer << source_file.rdbuf();
+	std::string code = buffer.str();
 	
-	:recall
-	READ_STACK
-	SWAP
-	PREPARE_ARGS
-	PUSH_STACK
-	CALL :print_num_inner 1
-	POP_STACK
-	RET
+	ASM::Compiler compiler;
+	compiler.append(code);
 
-:print_num_inner
-	READ_STACK
-	
-	JUMP_NOT_ZERO :mainline
-	POP_STACK
-	RET
+	Hardware::Computer computer(compiler.get_memory());
 
-	:mainline
-	READ_STACK
-	SET 10
-	SWAP
-	READ_STACK
-	DIV
-	SWAP
-
-	PREPARE_ARGS
-	PUSH_STACK
-	CALL :print_num_inner 1
-	
-	SET 10
-	SWAP
-	READ_STACK
-	MOD
-	SWAP
-
-	SET '0'
-	ADD
-	SWAP
-	PRINT
-
-	POP_STACK
-	RET
-)code"; //expected "42069"
-	
-	ASM::Compiler compiler(asm_code);
-	auto machine_code = compiler.compile();
-	
-	Hardware::Computer computer(machine_code);
-	computer.run(10000);
+	computer.execute_step(compiler.get_mapping());
+	auto segment = compiler.get_mapping()[computer.processor->get_IP()];
+	std::cerr << "ASM code: " << code.substr(segment.from, segment.to - segment.from) << "\n";
+	//need to be second instruction, because first already done. (SET 42069)
 }
