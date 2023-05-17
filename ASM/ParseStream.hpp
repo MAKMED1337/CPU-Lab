1de6393c8fdb0ed1ad3f75ff047be78cf3f4f3e7
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-05-17 02:05:23 +0200

    Now supports offset, used as proxy not as inheritance

--- ASM/ParseStream.hpp
+++ ASM/ParseStream.hpp
@@ -8,12 +8,25 @@ public:
 	TokenException(const std::string& expected, const std::string& got);
 };
 
-class ParseStream final : public std::string_view {
+class ParseStream final {
+	std::string_view m_string;
+	size_t m_offset = 0;
 public:
+	ParseStream() = default;
 	ParseStream(std::string_view s);
-	
+	void set(std::string_view s);
+
 	//consumes if true
 	bool test(std::string_view s);
-	
 	void expect(std::string_view s);
+	size_t offset() const;
+
+	void remove_prefix(size_t n);
+	bool empty() const;
+	char front() const;
+	char operator[] (size_t index) const;
+	ParseStream substr(size_t pos, size_t n) const;
+	size_t size() const;
+
+	operator std::string_view() const;
 };
